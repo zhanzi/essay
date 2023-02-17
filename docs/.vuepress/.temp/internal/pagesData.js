032c3feea51ac3d491fb6809c9a6617df6a33dbd
zhanzi <zhanzi124@hotmail.com>
2023-02-17 14:01:35 +0800

    vuepress init

---
+++ docs/.vuepress/.temp/internal/pagesData.js
@@ -0,0 +1,6 @@
+export const pagesData = {
+  // path: /
+  "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"E:/Coding/learn/Blog/essay/docs/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
+  // path: /404.html
+  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"E:/Coding/learn/Blog/essay/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
+}
---
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -0,0 +1,30 @@
+<template><div><h2 id="welcome-to-github-pages" tabindex="-1"><a class="header-anchor" href="#welcome-to-github-pages" aria-hidden="true">#</a> Welcome to GitHub Pages</h2>
+<p>You can use the <a href="https://github.com/zhanzi/essay/edit/master/README.md" target="_blank" rel="noopener noreferrer">editor on GitHub<ExternalLinkIcon/></a> to maintain and preview the content for your website in Markdown files.</p>
+<p>Whenever you commit to this repository, GitHub Pages will run <a href="https://jekyllrb.com/" target="_blank" rel="noopener noreferrer">Jekyll<ExternalLinkIcon/></a> to rebuild the pages in your site, from the content in your Markdown files.</p>
+<h3 id="markdown" tabindex="-1"><a class="header-anchor" href="#markdown" aria-hidden="true">#</a> Markdown</h3>
+<p>Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for</p>
+<div class="language-markdown line-numbers-mode" data-ext="md"><pre v-pre class="language-markdown"><code>Syntax highlighted code block
+
+<span class="token title important"><span class="token punctuation">#</span> Header 1</span>
+<span class="token title important"><span class="token punctuation">##</span> Header 2</span>
+<span class="token title important"><span class="token punctuation">###</span> Header 3</span>
+
+<span class="token list punctuation">-</span> Bulleted
+<span class="token list punctuation">-</span> List
+
+<span class="token list punctuation">1.</span> Numbered
+<span class="token list punctuation">2.</span> List
+
+<span class="token bold"><span class="token punctuation">**</span><span class="token content">Bold</span><span class="token punctuation">**</span></span> and <span class="token italic"><span class="token punctuation">_</span><span class="token content">Italic</span><span class="token punctuation">_</span></span> and <span class="token code-snippet code keyword">`Code`</span> text
+
+<span class="token url">[<span class="token content">Link</span>](<span class="token url">url</span>)</span> and <span class="token url"><span class="token operator">!</span>[<span class="token content">Image</span>](<span class="token url">src</span>)</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>For more details see <a href="https://guides.github.com/features/mastering-markdown/" target="_blank" rel="noopener noreferrer">GitHub Flavored Markdown<ExternalLinkIcon/></a>.</p>
+<h3 id="jekyll-themes" tabindex="-1"><a class="header-anchor" href="#jekyll-themes" aria-hidden="true">#</a> Jekyll Themes</h3>
+<p>Your Pages site will use the layout and styles from the Jekyll theme you have selected in your <a href="https://github.com/zhanzi/essay/settings" target="_blank" rel="noopener noreferrer">repository settings<ExternalLinkIcon/></a>. The name of this theme is saved in the Jekyll <code v-pre>_config.yml</code> configuration file.</p>
+<h3 id="support-or-contact" tabindex="-1"><a class="header-anchor" href="#support-or-contact" aria-hidden="true">#</a> Support or Contact</h3>
+<p>Having trouble with Pages? Check out our <a href="https://help.github.com/categories/github-pages-basics/" target="_blank" rel="noopener noreferrer">documentation<ExternalLinkIcon/></a> or <a href="https://github.com/contact" target="_blank" rel="noopener noreferrer">contact support<ExternalLinkIcon/></a> and we’ll help you sort it out.
+<img src="@source/img/2019-06-12-00-25-40.png" alt="a pic"></p>
+<p>there is a pic</p>
+</div></template>
+
+
